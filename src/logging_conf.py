1593d43cd099e0770870c035a51e7264c030e5a5
Cortland Goffena <30168413+cmgoffena13@users.noreply.github.com>
2025-11-13 18:27:29 -0700

    updating to show traces and logs

--- src/logging_conf.py
+++ src/logging_conf.py
@@ -1,25 +1,42 @@
 from logging.config import dictConfig
 
+from opentelemetry import trace
 from opentelemetry._logs import set_logger_provider
 from opentelemetry.exporter.otlp.proto.http._log_exporter import OTLPLogExporter
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
 from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 from src.settings import ProdConfig, config
 
 
 def setup_logging():
+    # Setup OpenTelemetry tracing
+    tracer_provider = TracerProvider()
+    trace.set_tracer_provider(tracer_provider)
+
+    # Setup OpenTelemetry logging
     logger_provider = LoggerProvider()
     set_logger_provider(logger_provider)
 
     if isinstance(config, ProdConfig):
-        # Setup OpenTelemetry logging for Production
-        exporter = OTLPLogExporter(
-            endpoint=config.OPEN_TELEMETRY_ENDPOINT,
+        # Setup OpenTelemetry tracing exporter
+        trace_exporter = OTLPSpanExporter(
+            endpoint=config.OPEN_TELEMETRY_TRACE_ENDPOINT,
             headers={"Authorization": config.OPEN_TELEMETRY_AUTHORIZATION_TOKEN},
         )
-        processor = BatchLogRecordProcessor(exporter)
-        logger_provider.add_log_record_processor(processor)
+        trace_processor = BatchSpanProcessor(trace_exporter)
+        tracer_provider.add_span_processor(trace_processor)
+
+        # Setup OpenTelemetry logging exporter
+        log_exporter = OTLPLogExporter(
+            endpoint=config.OPEN_TELEMETRY_LOG_ENDPOINT,
+            headers={"Authorization": config.OPEN_TELEMETRY_AUTHORIZATION_TOKEN},
+        )
+        log_processor = BatchLogRecordProcessor(log_exporter)
+        logger_provider.add_log_record_processor(log_processor)
 
         handlers = {
             "default": {
